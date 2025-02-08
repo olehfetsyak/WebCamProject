2e88cc8f3998657bfc34367cae94bc54f93a8ef9
olehfetsyak <olehfetsyak@gmail.com>
2025-02-08 09:22:45 -0800

    Modified to work with MJPEG rather then GIF images Created Countour line Generator (With help of AI) Masked Countour + Original Image

--- colorQuantifier.py
+++ colorQuantifier.py
@@ -2,13 +2,13 @@ from math import sqrt
 
 class ColorQuantifier:
     def __init__(self, Light1, GreenRed1, YellowBlue1, Light2, GreenRed2, YellowBlue2):
-        self.L1 = Light1
-        self.a1 = GreenRed1
-        self.b1 = YellowBlue1
+        self.L1 = int(Light1)
+        self.a1 = int(GreenRed1)
+        self.b1 = int(YellowBlue1)
         
-        self.L2 = Light2
-        self.a2 = GreenRed2
-        self.b2 = YellowBlue2
+        self.L2 = int(Light2)
+        self.a2 = int(GreenRed2)
+        self.b2 = int(YellowBlue2)
 
     def calculateDeltaE(self):
         return sqrt((self.L2 - self.L1) ** 2 + (self.a2 - self.a1) ** 2 + (self.b2 - self.b1) ** 2)
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PIL import Image
 import numpy
 import cv2
 
-vid_path = '/Users/pyProject/WebCamProject/testCam.gif'
+
+vid_path = '/Users/pyProject/WebCamProject/testCam.mjpeg.avi'
 
 def colorDifference(color1, color2): 
     colorDifference = ColorQuantifier(
@@ -15,62 +16,69 @@ def colorDifference(color1, color2):
         color2[1], 
         color2[2],   
     )
-    deltaE = colorDifference.calculateDeltaE()
-    return 0 if colorDifference.calculateDeltaE() < 2 else 1
+    #deltaE = colorDifference.calculateDeltaE()
+    return 0 if colorDifference.calculateDeltaE() < 5 else 1
 
 
 #-------------------- Break --------------------#
 
 
 def getImgArray(frame_number, vid_path, max_width):
-    with Image.open(vid_path) as img:
-        width, height = img.size
-        aspect_ratio = height / width
+    capture = cv2.VideoCapture(vid_path)
 
-        img.seek(frame_number)
-        resized_img1 = img.resize((max_width, int(max_width * aspect_ratio)), Image.Resampling.LANCZOS)
-        pixel1 = numpy.asarray(resized_img1, dtype=numpy.uint8).copy()
-        pixel1= pixel1[..., :3]
-        pixel1 = cv2.cvtColor(pixel1, cv2.COLOR_RGB2BGR)
-        pixel1  = cv2.cvtColor(pixel1, cv2.COLOR_BGR2LAB)
+    capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    ret, pixel1 = capture.read()
+    capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number + 1)
+    ret, pixel2 = capture.read()
 
-        img.seek(frame_number + 1)
-        resized_img2 = img.resize((max_width, int(max_width * aspect_ratio)), Image.Resampling.LANCZOS)
-        pixel2 = numpy.asarray(resized_img2, dtype=numpy.uint8).copy()
-        pixel2 = pixel2[..., :3]
-        pixel2 = cv2.cvtColor(pixel2, cv2.COLOR_RGB2BGR)
-        pixel2 = cv2.cvtColor(pixel2, cv2.COLOR_BGR2LAB)
+    cv2.imwrite(f'./img/frame{frame_number}.jpg', pixel1)
 
-        return [pixel1, pixel2]
-    
+    capture.release()
+
+    height, width, _ = pixel1.shape
+    aspect_ratio = height / width
+    pixel1 = cv2.resize(pixel1, (max_width, int(max_width * aspect_ratio)), interpolation=cv2.INTER_LANCZOS4)
+    pixel2 = cv2.resize(pixel2, (max_width, int(max_width * aspect_ratio)), interpolation=cv2.INTER_LANCZOS4)
+
+    pixel1 = cv2.cvtColor(pixel1, cv2.COLOR_BGR2LAB)
+    pixel2 = cv2.cvtColor(pixel2, cv2.COLOR_BGR2LAB)
+
+    return [pixel1, pixel2]
+
+def generateMask(mask_image_path, image_path):
+    mask_image = cv2.imread(mask_image_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(image_path)
+
+    mask_image = cv2.resize(mask_image, (image.shape[1], image.shape[0]))
+
+    mask = cv2.inRange(mask_image, 1, 255)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    result = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+    return result
 
 for i in range(8):
-    pixelArray = getImgArray(i, vid_path, 20)
+    pixel_array = getImgArray(i, vid_path, 80)
 #print(f"\nFirst Frame:\n{pixelArray[0]}")
 #print(f"\nSecond Frame:\n{pixelArray[1]}")
 
     new_image = []
 
-    for x in range(pixelArray[0].shape[0]):
+    for x in range(pixel_array[0].shape[0]):
         row = []
-        for y in range(pixelArray[0].shape[1]):
+        for y in range(pixel_array[0].shape[1]):
             difference = colorDifference(
-                pixelArray[0][x][y], 
-                pixelArray[1][x][y]
+                pixel_array[0][x][y], 
+                pixel_array[1][x][y]
             )
             row.append(difference)
         new_image.append(row)
-            
-
-
-
+    #print(new_image)
     array = numpy.array(new_image, dtype=numpy.uint8)
     image = Image.fromarray(array * 255, mode="L") 
 
-    image.save(f"./WebCamProject/img/{i}.png")
+    image.save(f"./img/{i}.png")
 
-
-
-
-
+    cv2.imwrite(f'./img/result{i}.png', generateMask(f'./img/{i}.png', f'./img/frame{i}.jpg'))
 
